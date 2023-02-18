ebaaf2d06a37665be593894821d4ee7101e9e04a
elguesabal <113626409+elguesabal@users.noreply.github.com>
2023-02-17 21:31:13 -0300

    Update index.js
    requisicoes adicionada:
    
    cadastrar paciente
    ver informacoes do paciente
    editar informacoes do paciente
    excluir paciente

--- Trabalho/index.js
+++ Trabalho/index.js
@@ -42,7 +42,119 @@ app.get('/', (req, res) => {
 
                     // AKI FICA O ESPACO PARA AS REQUISICOES
 
+        // CADASTRAR PACIENTE   -   JOSE
+app.get('/cadastrar', (req, res) => {
+    res.render('cadastrar', {layout: false})
+})
 
+app.post('/novoCliente',(req, res) => {
+    const nome = req.body.nome
+    const nascimento = req.body.nascimento
+    const email = req.body.email
+    const telefone = req.body.telefone
+    const cpf = req.body.cpf
+    const senha = req.body.senha
+
+    const sql = `INSERT INTO paciente (nome, nascimento, email, telefone, cpf, senha) VALUES ('${nome}', '${nascimento}', '${email}', '${telefone}', '${cpf}', '${senha}')`
+
+    conn.query(sql, function(err) {
+        if (err) {
+            console.log(err)
+        }
+
+        res.redirect('/')
+    })
+})
+        // CADASTRAR PACIENTE   -   JOSE
+
+
+        // VER INFORMACOES PACIENTE   -   JOSE
+app.post('/cliente', (req, res) => {
+    const cpf = req.body.cpf
+    
+    const sql = `SELECT * FROM paciente WHERE cpf = '${cpf}'`
+
+    conn.query(sql, function(err, data){
+        if(err){
+            console.log(err)
+            return
+        }
+
+        res.redirect(`/cliente/${cpf}`)
+    })
+})
+
+app.get('/cliente/:cpf', (req, res) => {
+    const cpf = req.params.cpf
+    const sql = `SELECT * FROM paciente WHERE cpf = '${cpf}'`
+
+    conn.query(sql, function(err, data) {
+        if (err) {
+            console.log(err)
+            return
+        }
+
+        const cliente = data[0]
+        res.render('clienteInfo', {layout: false, cliente})
+    })
+})
+        // VER INFORMACOES PACIENTE   -   JOSE
+
+
+        // EDITAR INFORMACOES PACIENTE   -   JOSE
+app.get('/cliente/editar/:cpf', (req, res) => {
+    const cpf = req.params.cpf
+    const sql = `SELECT * FROM paciente WHERE cpf = '${cpf}'`
+
+    conn.query(sql, function(err, data) {
+        if(err) {
+            console.log(err)
+            return
+        }
+
+        const cliente = data[0]
+        res.render('clienteEditar', {layout: false, cliente})
+    })
+})
+
+app.post('/cliente/edit', (req, res) => {
+    const nome = req.body.nome
+    const nascimento = req.body.nascimento
+    const email = req.body.email
+    const telefone = req.body.telefone
+    const cpf = req.body.cpf
+    const senha = req.body.senha
+
+    const sql = `UPDATE paciente SET nome = '${nome}', nascimento = '${nascimento}', email = '${email}', telefone = '${telefone}', senha = '${senha}' WHERE cpf = '111111'`
+
+    conn.query(sql, function(err) {
+        if (err) {
+            console.log(err)
+            return
+        }
+
+        res.redirect(`/cliente/${cpf}`)
+    })
+})
+        // EDITAR INFORMACOES PACIENTE   -   JOSE
+
+
+        // EXCLUIR PACIENTE  -   JOSE
+app.get('/cliente/excluir/:cpf', (req, res) => {
+    const cpf = req.params.cpf
+
+    const sql = `DELETE FROM paciente WHERE cpf = '${cpf}'`
+
+    conn.query(sql, function(err) {
+        if (err) {
+            console.log(err)
+            return
+        }
+
+        res.redirect('/')
+    })
+})
+        // EXCLUIR PACIENTE  -   JOSE
 
 
 

3b19729b3a01ad22d8082891ab94726a0fdde8d0
Hoptavio <114100449+Hoptavio@users.noreply.github.com>
2023-03-09 13:25:24 -0300

    Update clinica_resilia.sql

--- database/clinica_resilia.sql
+++ database/clinica_resilia.sql
@@ -181,6 +181,7 @@ CREATE TABLE `receita` (
   `data` varchar(255) NOT NULL,
   `medico` varchar(255) NOT NULL,
   `remedio` varchar(255) NOT NULL
+  `prescricao` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
